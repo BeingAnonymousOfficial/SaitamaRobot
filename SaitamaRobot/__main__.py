8e4f1a0990fa903a6dc342aa3fd83dbf605d8946
BeingAnonymousOfficial <62430753+BeingAnonymousOfficial@users.noreply.github.com>
2020-06-19 15:38:45 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -21,6 +21,9 @@ Hi {}, my name is {}!
 
 I am a group manager bot maintained by @Abhinav_Official 
 
+To add me to your group click ["Below"]
+t.me/Shinchan_Robot?startgroup=botstart
+
 You can find my list of available commands with /help.
 
 My future updates will be put into This Channel - @ShinchanNews.
